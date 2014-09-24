3dc448d63828d65c6a4763bfa201b8c7a58ae472
priyatam <priyatam@gmail.com>
2014-09-23 23:03:40 -0700

    highlight js fix

--- src/js/design.js
+++ src/js/design.js
@@ -5,7 +5,7 @@ $(function() {
     // http://css-tricks.com/snippets/jquery/smooth-scrolling/
 
     $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
